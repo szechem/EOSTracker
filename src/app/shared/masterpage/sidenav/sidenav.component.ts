585e54fb97e2f53750f971b8722d694262b64451
motrebski <motrebski@syncad.com>
2019-01-10 11:58:26 +0100

    Hide wallet and voting from side nav

--- src/app/shared/masterpage/sidenav/sidenav.component.ts
+++ src/app/shared/masterpage/sidenav/sidenav.component.ts
@@ -46,18 +46,6 @@ export class SidenavComponent implements OnInit {
         icon: 'settings',
         link: '/settings',
         name: this.translate.instant('Settings')
-      },
-      {
-        icon: 'account_balance_wallet',
-        external: true,
-        link: environment.walletUrl,
-        name: this.translate.instant('Wallet')
-      },
-      {
-        icon: 'how_to_vote',
-        external: true,
-        link: environment.votingUrl,
-        name: this.translate.instant('Voting')
       }
     ];
   }
