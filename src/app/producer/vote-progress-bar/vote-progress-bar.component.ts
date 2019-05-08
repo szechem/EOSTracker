89277906d48b50deb8e7801e6a36db7fa70785ae
motrebski <mateuszotrebski1z@gmail.com>
2019-05-08 17:24:36 +0200

    Fix votes displaying

--- src/app/producer/vote-progress-bar/vote-progress-bar.component.ts
+++ src/app/producer/vote-progress-bar/vote-progress-bar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnChanges, Input } from '@angular/core';
+import * as Eos from 'eosjs';
+import { environment } from '../../../environments/environment.prod';
+import { Observable, from, BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-vote-progress-bar',
@@ -10,13 +13,33 @@ export class VoteProgressBarComponent implements OnChanges {
   @Input() chainStatus;
   chainPercentage;
   chainNumber;
+  private apiEndpointSource = new BehaviorSubject<string>(environment.blockchainUrl);
 
-  constructor() { }
+  public apiEndpoint$ = this.apiEndpointSource.asObservable();
+  public eos: any;
+  public supply: any;
+
+  constructor() {
+    this.apiEndpoint$.subscribe(apiEndpoint => {
+      this.eos = Eos({
+        httpEndpoint: apiEndpoint,
+        blockId: environment.chainId
+      });
+    });
+   }
 
   ngOnChanges() {
-    if (this.chainStatus) {
-      this.chainPercentage = (this.chainStatus.total_activated_stake / 10000 / 1000011818 * 100).toFixed(2);
-      this.chainNumber = (this.chainStatus.total_activated_stake / 1000011818 * 100000);
+    const allBeosTokens$: Observable<any[]> = from(this.eos.getCurrencyStats('eosio.token', 'BEOS'));
+    allBeosTokens$.subscribe(
+      (data) => {
+        if (data['BEOS']) {
+          this.supply = data['BEOS'].supply.split(' ')[0];
+        }
+      }
+    );
+    if (this.chainStatus && this.supply) {
+      this.chainPercentage = (this.chainStatus.total_activated_stake / 10000 / this.supply * 100).toFixed(2);
+      this.chainNumber = this.supply;
     }
   }
 
